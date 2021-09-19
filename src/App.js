9ad5a2e43f78adf92a4b7e0db6c7b09eac8d35ab
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-19 10:56:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    { name: 'phone', price: '14560', color: 'black blue' },
+    { name: 'laptop', price: '52000', color: 'nevy blue' },
+    { name: 'watch', price: '4360', color: 'black' },
+    { name: 'camera', price: '13460', color: 'lightblue' },
+    { name: 'shoe', price: '5670', color: 'dark blue' },
+    { name: 't-shirt', price: '530', color: 'black' },
+    { name: 'orange', price: '565', color: 'orange' },
+    { name: 'apple', price: '865', color: 'lightred' },
+    { name: 'banana', price: '65', color: 'orange' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App product-container">
+      {
+        products.map(product => <Product name={product.name} price={product.price} color={product.color}></Product>)
+      }
+
+
+      {/* <Product name='phone 📱' price='17000' color='black blue'></Product>
+      <Product name='laptop 💻' price='70000' color='black'></Product>
+      <Product name='watch ⌚' price='7000' color='blue'></Product>
+      <Product name='camera 📷' price='10500' color='coral'></Product>
+      <Product name='t-shirt 👕' price='250' color='white'></Product>
+      <Product name='shoe 👟' price='80' color='orange'></Product> */}
     </div>
   );
 }
-
+function Product(props) {
+  const productStyle = {
+    color: 'white'
+  }
+  return (
+    <div style={productStyle} className='product'>
+      <h2>Name: {props.name}</h2>
+      <h4>color: {props.color}</h4>
+      <h5>Price: {props.price}</h5>
+    </div>
+  );
+}
 export default App;
